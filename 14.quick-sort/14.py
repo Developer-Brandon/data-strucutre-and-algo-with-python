147a9542f0d2557030848e1ca1c536b35f4df657
brandonLee <lkc2252@gmail.com>
2020-10-03 16:19:31 +0900

    - Quick sort finished

--- 14.quick-sort/14.py
+++ 14.quick-sort/14.py
@@ -3,11 +3,12 @@ import random
 def quick_sort(list):
     if len(list) <= 1:
         return list
-    pivot = list[0]
-    left = [item for item in list[1:] if pivot > item]
-    right = [item for item in list[1:] if pivot < item]
-    return quick_sort(left) + [pivot] + quick_sort(right)
+    else:
+        standardValue = list[0]
+        left_list = [item for item in list if standardValue > item]
+        right_list = [item for item in list if standardValue < item]
+        return quick_sort(left_list) + [standardValue] + quick_sort(right_list)
 
-random_list = random.sample(range(100),10)
-print('Random list: ', random_list)
-print('After adjusting random list: ', quick_sort(random_list))
+random_list = random.sample(range(100), 50)
+print('\nRandom list: ', random_list)
+print('\nAfter quick sort: ', quick_sort(random_list))
